88df014d9c96407a96c28f0b727a816eb6511790
Jeorge Santos <jemjem.santos03@gmail.com>
2023-04-25 20:27:34 +0800

    fixed bug where sidebar button click is not closing

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-var sidemenu = document.getElementById("sidemenu");
+const sidemenu = document.getElementById('sidemenu');
 
 function closemenu() {
-  sidemenu.style.right = "-200px";
+  sidemenu.style.right = '-200px';
 }
 
 function openmenu() {
-  sidemenu.style.right = "0";
+  sidemenu.style.right = '0';
 }
 
-const formSent = document.getElementById("success");
+const formSent = document.getElementById('success');
 
 const scriptURL =
-  "https://script.google.com/macros/s/AKfycbzeNL2XVkww3t-49jL5HH0k8F8mUf123CuXkeWEhGmZUtD-Rs18cBCPFeKV3K7tRM9z/exec";
-const form = document.forms["submit-to-google-sheet"];
+  'https://script.google.com/macros/s/AKfycbzeNL2XVkww3t-49jL5HH0k8F8mUf123CuXkeWEhGmZUtD-Rs18cBCPFeKV3K7tRM9z/exec';
+const form = document.forms['submit-to-google-sheet'];
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then((response) => {
-      formSent.innerHTML = "Message sent successfully!";
+      formSent.innerHTML = 'Message sent successfully!';
       form.reset();
       setTimeout(() => {
-        formSent.innerHTML = "";
+        formSent.innerHTML = '';
       }, 5000);
     })
-    .catch((error) => console.error("Error!", error.message));
+    .catch((error) => console.error('Error!', error.message));
 });
